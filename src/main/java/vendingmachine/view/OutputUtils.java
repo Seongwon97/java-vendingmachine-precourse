eb4408259284faf34bce2060fa1afb74cb16e44f
Seongwon97 <osw970325@naver.com>
2021-12-13 00:23:13 +0900

    fix : 출력 타입에 맞게 수정

--- src/main/java/vendingmachine/view/OutputUtils.java
+++ src/main/java/vendingmachine/view/OutputUtils.java
@@ -4,8 +4,11 @@ import vendingmachine.domain.Coin;
 
 import java.util.Map;
 
+import static vendingmachine.constant.Constant.PRINT_MACHINE_COIN_MESSAGE;
+
 public class OutputUtils {
     public static void printMachineInitCoin(Map<Coin, Integer> remainCoin) {
+        System.out.println(PRINT_MACHINE_COIN_MESSAGE);
         for (Coin c: remainCoin.keySet()) {
             System.out.println(c.getAmount() + " - " + remainCoin.get(c) + "개");
         }
