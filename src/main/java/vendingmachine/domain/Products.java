4bbe570050e13ba0221eedf1be1d66049b410a64
Seongwon97 <osw970325@naver.com>
2021-12-12 18:43:32 +0900

    feat(Products) : 상품 클래스 생성
    - Product.java 클래스 생성
    - Products.java 클래스 생성

---
+++ src/main/java/vendingmachine/domain/Products.java
@@ -0,0 +1,31 @@
+package vendingmachine.domain;
+
+import java.util.List;
+import java.util.Map;
+
+public class Products {
+    private Map<String, Product> productList;
+
+    public Products(Map<String, Product> productList) {
+        this.productList = productList;
+    }
+
+    public void consume(String name) {
+        if (!isExist(name)) {
+            return;
+        }
+        Product product = productList.get(name);
+        product.setQuantity(product.getQuantity() - 1);
+    }
+
+    private boolean isExist(String name) {
+        if (!productList.containsKey(name)) {
+            return false;
+        }
+        if (productList.get(name).getQuantity() <= 0) {
+            return false;
+        }
+        return true;
+    }
+
+}
