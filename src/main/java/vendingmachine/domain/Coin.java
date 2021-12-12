a01d639b9aa56c58e2117a9af60bacaf5bdbc5b2
Seongwon97 <osw970325@naver.com>
2021-12-12 23:02:20 +0900

    feat(VendingMachine) : 자판기가 보유한 초기 동전 설정 기능 추가
    - initRemainCoin(): remainCoin Map을 초기화 한 후 setRemainCoin()호출
    - setRemainCoin() : 남은 돈이 0이 될 때까지 coin을 랜덤하게 뽑아서 Map에 추가
    - pickRandomCoin() : 랜덤하게 코인 하나를 선택
    - checkAddCoinAvailable() : 남은 돈과 비교하여 선택된 코인을 추가할 수 있는지 판별

--- src/main/java/vendingmachine/domain/Coin.java
+++ src/main/java/vendingmachine/domain/Coin.java
@@ -1,5 +1,10 @@
 package vendingmachine.domain;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public enum Coin {
     COIN_500(500),
     COIN_100(100),
@@ -13,4 +18,27 @@ public enum Coin {
     }
 
     // 추가 기능 구현
+    public static List<Coin> getCoinList() {
+        return Arrays.stream(Coin.values())
+                .sequential()
+                .collect(Collectors.toList());
+    }
+
+    public static List<Integer> getCoinValueList() {
+        return Arrays.stream(Coin.values())
+                .map(Coin::getAmount)
+                .sequential()
+                .collect(Collectors.toList());
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public static Coin getEnumCoin(int amount) {
+        return Arrays.stream(Coin.values())
+                .filter(coin -> coin.getAmount() == amount)
+                .sequential().collect(Collectors.toList()).get(0);
+    }
+
 }
--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -1,24 +1,57 @@
 package vendingmachine.domain;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInList;
+import static vendingmachine.domain.Coin.*;
 
 public class VendingMachine {
 
-    Map<Coin, Integer> remainCoin = new HashMap<>();
-    Products products;
-    int change;
+    private Map<Coin, Integer> remainCoin = new TreeMap<>();
+    public Products products;
+    private int initMoney;
 
-    public VendingMachine(int change, Products products) {
+    public VendingMachine(int initMoney, Products products) {
+        initRemainCoin(initMoney);
         this.products = products;
-        this.change = change;
+        this.initMoney = initMoney;
     }
 
-    public int getChange() {
-        return change;
+    public int getInitMoney() {
+        return initMoney;
     }
 
     public Map<Coin, Integer> getRemainCoin() {
         return remainCoin;
+    }
+
+    private void initRemainCoin(int initMoney) {
+        for (Coin c: getCoinList()) {
+            remainCoin.put(c, 0);
+        }
+        setRemainCoin(initMoney);
+    }
+
+    private void setRemainCoin(int initMoney) {
+        int remainMoney = initMoney;
+        while (remainMoney > 0) {
+            int pickedCoin = pickRandomCoin();
+            if (checkAddCoinAvailable(remainMoney, pickedCoin)) {
+                remainMoney -= pickedCoin;
+                remainCoin.put(getEnumCoin(pickedCoin), remainCoin.get(getEnumCoin(pickedCoin))+1);
+            }
+        }
+    }
+
+    private static int pickRandomCoin() {
+        return pickNumberInList(getCoinValueList());
+    }
+
+    private static boolean checkAddCoinAvailable(int remainMoney, int coin) {
+        if (remainMoney < coin) {
+            return false;
+        }
+        return true;
     }
 }
